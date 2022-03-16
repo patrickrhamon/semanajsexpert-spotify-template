695dfdff0b53ca003224f893279a53f25ce1bafc
Patrick Rhamon R. Miranda <prr.miranda@gmail.com>
2022-03-15 23:28:08 -0300

    Aula 1

---
+++ server/service.js
@@ -0,0 +1,44 @@
+import fs from 'fs'
+import fsPromises from 'fs/promises'
+
+import { join, extname } from 'path'
+import config from './config.js'
+
+const {
+    dir: {
+        publicDirectory
+    }
+} = config
+
+export class Service {
+    createFileStram(filename) {
+        return fs.createReadStream(filename)
+    }
+
+    async getFileInfo(file) {
+        // file = home/index.html
+        const fullFilePath = join(publicDirectory, file)
+
+        //valida dse existe, se não existe estoura erro!!
+        await fsPromises.access(fullFilePath)
+
+        const fileType = extname(fullFilePath)
+
+        return {
+            type: fileType,
+            name: fullFilePath
+        }
+    }
+
+    async getFileStream(file) {
+        const {
+            type,
+            name
+        } = await this.getFileInfo(file)
+
+        return {
+            stream: this.createFileStram(name),
+            type
+        }
+    }
+}
---
+++ tests/unit/public/routes.test.js
@@ -0,0 +1,7 @@
+import { jest, expect, describe, test } from '@jest/globals'
+
+describe('#Routes', () => {
+    test('my Test', () => {
+        expect(true).toBeTruthy()
+    })
+})
